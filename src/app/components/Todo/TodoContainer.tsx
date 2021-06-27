91a5a8e77ef5e8285d085753605168636e40201f
Marlou Lopez <08.marlou.lopez@gmail.com>
2021-06-27 21:10:00 +0800

    break test

--- src/app/components/Todo/TodoContainer.tsx
+++ src/app/components/Todo/TodoContainer.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { v4 as uuid } from 'uuid';
 import { addTodo } from '../../redux/action-creators';
@@ -19,6 +20,12 @@ const TodoContainer: React.FC = () => {
     )
 
     setContent('')
+  }
+
+  const testf = () => {
+    useEffect(() => {
+      console.log('test')
+    },[])
   }
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
