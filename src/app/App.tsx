cba0fbea37ccfe0fb6d3abb31abfdfc2a13ac5d4
Marlou Lopez <08.marlou.lopez@gmail.com>
2021-06-28 01:17:44 +0800

    Add mui theme

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,18 @@
+import { CssBaseline, ThemeProvider } from '@material-ui/core';
 import React from 'react';
 import TodoContainer from './components/Todo/TodoContainer';
+import theme from './theme';
 
 const App: React.FC = () => {
+  console.log(theme)
   return (
-    <div>
-      <h3>Todo Too</h3>
-      <TodoContainer />
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div>
+        <h3 color='red'>Todo Too</h3>
+        <TodoContainer />
+      </div>
+    </ThemeProvider>
   )
 };
 
---
+++ src/app/theme.ts
@@ -0,0 +1,11 @@
+import { createMuiTheme } from "@material-ui/core";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#3E96C8'
+    }
+  }
+});
+
+export default theme;
