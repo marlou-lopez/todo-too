fcdcafeecfe871534b8a6e42b6f0faf2061c8685
Marlou Lopez <08.marlou.lopez@gmail.com>
2021-06-27 21:55:39 +0800

    Add build in workflow

--- src/app/components/Todo/TodoContainer.tsx
+++ src/app/components/Todo/TodoContainer.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { v4 as uuid } from 'uuid';
 import { addTodo } from '../../redux/action-creators';
